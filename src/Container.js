d603636f81398db5a49e526fe5afbe2538e4baa4
Olentia <pjh950117@gmail.com>
2022-10-18 20:50:45 +0900

    NewBalanceShop

--- src/Container.js
+++ src/Container.js
@@ -8,42 +8,49 @@ import 'swiper/css/effect-fade';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName } from './store.js';
 
 SwiperCore.use([Navigation, Pagination])
 
-function Container(props){
+function Container(){
 
-  let [banner1, setBanner1] = useState("");
-  let [title1, setTitle1] = useState("");
-  let [content1, setContent1] = useState("");
+  // let content = useSelector(( state )=>{ return state })
 
-  let [banner2, setBanner2] = useState("");
-  let [title2, setTitle2] = useState("");
-  let [content2, setContent2] = useState("");
-
-  let [video, setVideo] = useState("");
-  let [title3, setTitle3] = useState("");
-  let [content3, setContent3] = useState("");
+  let [banner, setBanner] = useState([]);
+  let [title, setTitle] = useState([]);
+  let [content, setContent] = useState([]);
 
   let [row2banner, setRow2banner] = useState("");
   let [row2title, setRow2title] = useState("");
   let [row2content, setRow2content] = useState("");
+
+  let [row3banner, setRow3banner] = useState("");
+  let [row3title, setRow3title] = useState("");
+  let [row3content, setRow3content] = useState("");
   
-  fetch(process.env.PUBLIC_URL + '/data/data.json')
+   fetch(process.env.PUBLIC_URL + '/data/data.json')
     .then(res => res.json())
     .then(res => {
-      setBanner1(res["data"][0]["url"]);
-      setTitle1(res["data"][0]["title"]);
-      setContent1(res["data"][0]["content"]);
-      setBanner2(res["data"][1]["url"]);
-      setTitle2(res["data"][1]["title"]);
-      setContent2(res["data"][1]["content"]);
-      setVideo(res["data"][2]["url"]);
-      setTitle3(res["data"][2]["title"]);
-      setContent3(res["data"][2]["content"]);
+      let banner = [];
+      let title = [];
+      let content = [];
+
+      for (let i = 0; i < res["data"].length; i++){
+        banner.push(res["data"][i]["url"]);
+        title.push(res["data"][i]["title"]);
+        content.push(res["data"][i]["content"]);
+      }
+
+      setBanner(banner);
+      setTitle(title);
+      setContent(content);
       setRow2banner(res["data"][3]["url"]);
       setRow2title(res["data"][3]["title"]);
       setRow2content(res["data"][3]["content"]);
+      setRow3banner(res["data"][4]["url"]);
+      setRow3title(res["data"][4]["title"]);
+      setRow3content(res["data"][4]["content"]);
   })
 
     return(
@@ -53,20 +60,15 @@ function Container(props){
           <a href="#">
             <Swiper modules={[EffectFade]} effect="fade" transformel="true" className="banner" spaceBetween={0} slidesPerView={1} navigation pagination={{ clickable: true }} 
                     autoPlay={{ delay: 2000, disableOnInteraction: false}} allowTouchMove={false}
-                    loop
-                    >
-                <SwiperSlide><img src={process.env.PUBLIC_URL + banner1} alt="banner1"/>
-                  <p className="title">{title1}</p>
-                  <p className="content">{content1}</p> 
-                </SwiperSlide>
-                <SwiperSlide><img src={process.env.PUBLIC_URL + banner2} alt="banner2"/>
-                  <p className="title">{title2}</p>
-                  <p className="content">{content2}</p> 
-                </SwiperSlide>
-                <SwiperSlide><video muted autoPlay loop src={process.env.PUBLIC_URL + video} type="video/mp4"></video>
-                  <p className="title">{title3}</p>
-                  <p className="content">{content3}</p> 
-                </SwiperSlide>   
+                    loop>
+              {[1, 2, 3].map((a, i) => {
+                return <>
+                <SwiperSlide key={a}>
+                  <img src={process.env.PUBLIC_URL + banner[i]} alt="banner" />
+                  <p className="title">{title[i]}</p>
+                  <p className="content">{content[i]}</p>  
+                </SwiperSlide></>
+              })}
             </Swiper>
           </a>
         </section>
@@ -81,12 +83,19 @@ function Container(props){
           <div className="text_box">
             <input type="submit" value="더 알아보기" name="more" className="submit_btn"></input>
             <input type="submit" value="구매하기" name="send" className="submit_btn2"></input>
-        </div>
+          </div>
         </section>
-        
+        <section className="row3">
+          <a href="#"><video muted autoPlay loop src={process.env.PUBLIC_URL + row3banner} alt="row3banner" /></a>
+          <p className="title">{row3title}</p>
+          <p className="content">{row3content}</p>
+          <div className="text_box">
+            <input type="submit" value="더 알아보기" name="more" className="submit_btn"></input>
+            <input type="submit" value="구매하기" name="send" className="submit_btn2"></input>
+          </div>
+        </section>
       </div>
     );
-
   }
 
 export default Container;
---
+++ src/store.js
@@ -0,0 +1,18 @@
+// import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+// let content = createSlice({
+//     name : "content",
+//     initialState : "NB WINDBREAKER",
+//     reducers : {
+//         changeName(){
+//             return "A Revolutionary Running Shoe with N-ergy"
+//         }
+//     }
+//   }
+// )
+
+// export default configureStore({
+//   reducer: { 
+//     content : content.reducer
+//   }
+// }) 
